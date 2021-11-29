4eed9214310b51ce2015c818be783f8a1ff02bdc
pedrowille <pedrowille9@gmail.com>
2021-11-29 09:55:30 -0300

    exercício 01

---
+++ exercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EstruturasRepeticao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+int maiorDezoito = 0;
+List<int> idades = new ();
+for (int i=1; i<=10; i++)                
+        {
+            Console.WriteLine($"Informe a idade da pessoa número {i}");
+        idades.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+foreach (int i in idades)
+{
+    if (i>=18)
+    {
+        maiorDezoito++;
+    }
+}
+Console.WriteLine($"{maiorDezoito} pessoas tem mais de dezoito anos.");
+        }
+    }
+}
